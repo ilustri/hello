db81cb476e39674e82c05701034022106fee5cc9
Carol <ilustri@ilustri.com>
2019-10-30 07:39:02 -0300

    Carol 2019-10-30 07:38

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -3,12 +3,6 @@ import Vapor
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
     // Your code here
-    var toff 		= "Carol 2019-10-29 12:45" 
-    var Marcos_1A 	= "Marcos 2019-10-29 22:55" 
-    var Marcos_1B 	= "Marcos 2019-10-29 22:56" 
-    var carol_1 	= "Carol 2019-10-29 22:54" 
 
-    var Marcos_2 	= "Marcos 2019-10-29 23:04" 
-    var carol_2 	= "Carol 2019-10-29 23:02" 
 
 }
