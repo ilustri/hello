fb0f6754276620dcb743aa78b9c2287d7aa9030e
Marcos Github <ilustri@ilustri.com>
2019-10-29 23:07:07 -0300

    Marcos 2019-10-29 23:04

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -4,10 +4,9 @@ import Vapor
 public func boot(_ app: Application) throws {
     // Your code here
     var toff 		= "Carol 2019-10-29 12:45" 
-
     var Marcos_1A 	= "Marcos 2019-10-29 22:55" 
     var Marcos_1B 	= "Marcos 2019-10-29 22:56" 
-
     var carol_1 	= "Carol 2019-10-29 22:54" 
 
+    var Marcos_2 	= "Marcos 2019-10-29 23:04" 
 }
