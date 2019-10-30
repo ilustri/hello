564176b25d8c791c3cbc77b82639ac8eb287dc29
Carol <ilustri@ilustri.com>
2019-10-29 22:55:02 -0300

    Carol 2019-10-29 22:54

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -12,4 +12,6 @@ public func boot(_ app: Application) throws {
     var toff 	= "Carol 2019-10-29 14:05" 
     var toff2 	= "Carol 2019-10-29 22:20" 
     var toff2B 	= "Carol 2019-10-29 22:42" 
+
+    var carol_1 = "Carol 2019-10-29 22:54" 
 }
