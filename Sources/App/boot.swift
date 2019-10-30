77555645ebc98f2f4d215fcc9ab970971f2d5077
Marcos Github <ilustri@ilustri.com>
2019-10-29 23:00:14 -0300

    Marcos 2019-10-29 22:59 Ajeitando

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -3,21 +3,11 @@ import Vapor
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
     // Your code here
-    var tof 	= "string"  
-    var toff 	= "28oct" 
-    var toff 	= "29oct" 
-    var toff 	= "Carol 2019-10-29 12:45" 
-    var toff 	= "Carol 2019-10-29 13:47" 
-    var toff 	= "Carol 2019-10-29 13:50" 
-    var toff 	= "Carol 2019-10-29 14:05" 
-    var toff2 	= "Carol 2019-10-29 22:20" 
-    var toff2B 	= "Carol 2019-10-29 22:42" 
+    var toff 		= "Carol 2019-10-29 12:45" 
 
-<<<<<<< HEAD
     var Marcos_1A 	= "Marcos 2019-10-29 22:55" 
     var Marcos_1B 	= "Marcos 2019-10-29 22:56" 
 
-=======
-    var carol_1 = "Carol 2019-10-29 22:54" 
->>>>>>> HelloRemoteOnline/master
+    var carol_1 	= "Carol 2019-10-29 22:54" 
+
 }
