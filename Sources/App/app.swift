daf05729c4f0902bcf5f78fc36d0767d0e95b2a1
Marcos Github <ilustri@ilustri.com>
2019-10-30 08:21:06 -0300

    INICIANDO

--- Sources/App/app.swift
+++ Sources/App/app.swift
@@ -1,20 +1,15 @@
 import Vapor
 
-/// Creates an instance of `Application`. This is called from `main.swift` in the run target.
 public func app(_ env: Environment) throws -> Application {
     var config = Config.default()
     var env = env
-    
-    //top
     var services = Services.default()
     try configure(&config, &env, &services)
     let app = try Application(config: config, environment: env, services: services)
     try boot(app)
+    // Testando Rebase
+    
 
-    var Carol4      = "2019-10-30 07:52"
-    var Carol5      = "2019-10-30 07:59"
 
-    var Carol6      = "Carol 2019-10-30 08:09"
-    // fim
     return app
 }
--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -1,15 +1,6 @@
 import Vapor
 
-/// Called after your application has initialized.
 public func boot(_ app: Application) throws {
-    // Your code here
-    var Marcos1 = "2019-10-30 07:40"
-    var Carol1 	= "2019-10-30 07:40"
-    var Carol2 	= "2019-10-30 07:44"
-    var Marcos2 = "2019-10-30 07:44"
-    var Carol3 	= "2019-10-30 07:49"
-    var Marcos4 = "2019-10-30 07:53"
-    var Marcos5 = "2019-10-30 08:04"
-
-    var Marcos6 = "Marcos 2019-10-30 08:04" 
+	// Testando Rebase
+ 
 }
